938ff3239424bc04dcef453dbf1b5b35228f4756
sonubee <notifications3972@gmail.com>
2016-02-23 22:39:47 -0800

    make it better3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,11 +34,7 @@ public class Main {
 			  "svp64bn3p56344yj",
 			  "fa458ae542e48d150ed2d456d28f16b7"
 			);
-    
-    public static String generateToken (){
-		token = "32323";
-		return token;
-	}
+   
 
   public static void main(String[] args) {
 	
@@ -46,15 +42,12 @@ public class Main {
     staticFileLocation("/public");
 
   		get("/hello", (request, response) ->{
-        	return "Hello World: ";
+        	return "Hello World!";
         });
-		
-		//get("client_token", (req, res) -> "598fsafnlk;asdjf;klawfoieuwrwe8f7u89wu7fg89sduafosdiofjoipas");
-	
         
         get("/client_token", (request, response) ->{
         	return "eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiIzN2Y4Zjg2ZWFhMjQxZWNjZGQ4MTg4ODFkNWQ5ZTUyNzlmNTg4OGEyNmM5MGM1N2QzMDc2ZmQzNDIxY2YwZGNlfGNyZWF0ZWRfYXQ9MjAxNi0wMi0yMFQyMzoxNDowNC4zMDgyMzkxNzUrMDAwMFx1MDAyNm1lcmNoYW50X2lkPTM0OHBrOWNnZjNiZ3l3MmJcdTAwMjZwdWJsaWNfa2V5PTJuMjQ3ZHY4OWJxOXZtcHIiLCJjb25maWdVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvMzQ4cGs5Y2dmM2JneXcyYi9jbGllbnRfYXBpL3YxL2NvbmZpZ3VyYXRpb24iLCJjaGFsbGVuZ2VzIjpbXSwiZW52aXJvbm1lbnQiOiJzYW5kYm94IiwiY2xpZW50QXBpVXJsIjoiaHR0cHM6Ly9hcGkuc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbTo0NDMvbWVyY2hhbnRzLzM0OHBrOWNnZjNiZ3l3MmIvY2xpZW50X2FwaSIsImFzc2V0c1VybCI6Imh0dHBzOi8vYXNzZXRzLmJyYWludHJlZWdhdGV3YXkuY29tIiwiYXV0aFVybCI6Imh0dHBzOi8vYXV0aC52ZW5tby5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tIiwiYW5hbHl0aWNzIjp7InVybCI6Imh0dHBzOi8vY2xpZW50LWFuYWx5dGljcy5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tIn0sInRocmVlRFNlY3VyZUVuYWJsZWQiOnRydWUsInRocmVlRFNlY3VyZSI6eyJsb29rdXBVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvMzQ4cGs5Y2dmM2JneXcyYi90aHJlZV9kX3NlY3VyZS9sb29rdXAifSwicGF5cGFsRW5hYmxlZCI6dHJ1ZSwicGF5cGFsIjp7ImRpc3BsYXlOYW1lIjoiQWNtZSBXaWRnZXRzLCBMdGQuIChTYW5kYm94KSIsImNsaWVudElkIjpudWxsLCJwcml2YWN5VXJsIjoiaHR0cDovL2V4YW1wbGUuY29tL3BwIiwidXNlckFncmVlbWVudFVybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS90b3MiLCJiYXNlVXJsIjoiaHR0cHM6Ly9hc3NldHMuYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhc3NldHNVcmwiOiJodHRwczovL2NoZWNrb3V0LnBheXBhbC5jb20iLCJkaXJlY3RCYXNlVXJsIjpudWxsLCJhbGxvd0h0dHAiOnRydWUsImVudmlyb25tZW50Tm9OZXR3b3JrIjp0cnVlLCJlbnZpcm9ubWVudCI6Im9mZmxpbmUiLCJ1bnZldHRlZE1lcmNoYW50IjpmYWxzZSwiYnJhaW50cmVlQ2xpZW50SWQiOiJtYXN0ZXJjbGllbnQzIiwiYmlsbGluZ0FncmVlbWVudHNFbmFibGVkIjp0cnVlLCJtZXJjaGFudEFjY291bnRJZCI6ImFjbWV3aWRnZXRzbHRkc2FuZGJveCIsImN1cnJlbmN5SXNvQ29kZSI6IlVTRCJ9LCJjb2luYmFzZUVuYWJsZWQiOmZhbHNlLCJtZXJjaGFudElkIjoiMzQ4cGs5Y2dmM2JneXcyYiIsInZlbm1vIjoib2ZmIn0=";
-        	//return 1;
+        
         });
         
         get("/client_token_real", (request, response) ->{
@@ -103,6 +96,10 @@ public class Main {
         System.out.println("1");
         String status = "";
         System.out.println("2");
+		
+		result.
+		
+		
         if (result.isSuccess() == true){
         	System.out.println("Success ");
         	Transaction transaction = result.getTarget();
@@ -112,7 +109,7 @@ public class Main {
         System.out.println("3");
         if (result.isSuccess() == false)
         {
-			/*
+			
         	System.out.println("Fail");
             Transaction transaction = result.getTransaction();
 
@@ -127,7 +124,7 @@ public class Main {
             System.out.println("Status: " + transaction.getStatus());
             System.out.println("Response Code: " + transaction.getProcessorResponseCode());
             System.out.println("Response Text: " + transaction.getProcessorResponseText());
-			*/
+			
         }
 
         System.out.println("4");
